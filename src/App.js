de013f329e23622a2fb50e91f3e03a4fbc443367
patrikcepr <wkidmusic@gmail.com>
2021-11-16 02:13:16 +0100

    useEffect beginning

--- src/App.js
+++ src/App.js
@@ -1,82 +1,12 @@
-import React, { Fragment, useReducer } from 'react';
+import React from 'react';
 
-import { Button } from '@mui/material';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        counter: state.counter + 1,
-        visible: state.visible,
-        text: state.text,
-      };
-    case 'VISIBLE':
-      return {
-        counter: state.counter,
-        visible: !state.visible,
-        text: state.text === 'SHOW' ? 'HIDE' : 'SHOW',
-      };
-    default:
-      return state;
-  }
-};
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, {
-    visible: true,
-    text: 'HIDE',
-    counter: 0,
-  });
-
-  // const [isVisible, setIsVisible] = useState({
-  //   visible: true,
-  //   text: 'HIDE',
-  //   counter: 0,
-  // });
-
-  const clickHandler = () => {
-    dispatch({ type: 'INCREMENT' });
-    dispatch({ type: 'VISIBLE' });
-    // setIsVisible((prevState) => {
-    //   return {
-    //     visible: !prevState.visible,
-    //     text: prevState.text === 'SHOW' ? 'HIDE' : 'SHOW',
-    //     counter: prevState.counter + 1,
-    //   };
-    // });
-  };
-
   return (
     <div className='App'>
-      <header className='App-header'>
-        {state.visible === true ? (
-          <Fragment>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Learn React
-            </a>
-          </Fragment>
-        ) : (
-          ''
-        )}
-        <Button
-          style={{ marginTop: '45px' }}
-          variant='outlined'
-          onClick={clickHandler}
-        >
-          {`${state.text}   ${state.counter}`}
-        </Button>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
